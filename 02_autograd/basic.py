b9eb667fdfdf1e8976737eb876aa3f7b2125ed3d
Faraz Alam Saifi <its.alam.faraz@gmail.com>
2026-07-02 15:42:23 +0530

    Module 02: detach, no_grad - autograd complete

--- 02_autograd/basic.py
+++ 02_autograd/basic.py
@@ -21,3 +21,23 @@ a.grad.zero_() # Reset the gradient to zero before the second backward pass
 c = a * 2
 c.backward()
 print("gradient after second backward: ", a.grad)
+print("--"*40)
+
+## now we will try detach() and no_grad() to avoid gradient accumulation
+
+d = torch.tensor(4.0, requires_grad=True)
+e = d ** 2
+
+f = e.detach()  # Detach e from the computation graph
+try:
+    f.backward()  # This will not compute gradients for d
+except RuntimeError as err:
+    print("Error during backward on detached tensor: ", err)
+print("gradient after detach: ", d.grad)
+
+## when we use no_grad(), it will not track the gradient inside the block
+with torch.no_grad():
+    g = d ** 3
+    
+    print(g.requires_grad)
+print("gradient after no_grad block: ", d.grad)
